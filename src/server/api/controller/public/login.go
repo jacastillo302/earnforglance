2bf8781dd8c66847fa4c815941301505d65cd76a
jacastillo302 <jacastillo302@gmail.com>
2025-05-05 15:42:03 -0500

    Add oauth2  library to login google

--- src/server/api/controller/public/login.go
+++ src/server/api/controller/public/login.go
@@ -1,22 +1,149 @@
 package controller
 
 import (
-	"net/http"
-	"strconv"
-
+	"context"
+	"crypto/rand"
 	"earnforglance/server/bootstrap"
 	common "earnforglance/server/domain/common"
 	custumers "earnforglance/server/domain/customers"
 	domain "earnforglance/server/domain/public"
 	security "earnforglance/server/domain/security"
 	service "earnforglance/server/service/security"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
+
+// Scopes: OAuth 2.0 scopes provide a way to limit the amount of access that is granted to an access token.
+var googleOauthConfig = &oauth2.Config{
+	RedirectURL:  "https://personasyrecursos.com/login_google_redirect",
+	ClientID:     os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
+	ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+	Endpoint:     google.Endpoint,
+}
 
 type LoginController struct {
 	LoginUsecase domain.LoginUsecase
 	Env          *bootstrap.Env
+}
+
+// GoogleClaims represents the claims returned by Google OAuth.
+type GoogleClaims struct {
+	Aud           string `json:"aud"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	Exp           int64  `json:"exp"`
+	FamilyName    string `json:"family_name"`
+	GivenName     string `json:"given_name"`
+	Iat           int64  `json:"iat"`
+	Iss           string `json:"iss"`
+	Locale        string `json:"locale"`
+	Name          string `json:"name"`
+	Picture       string `json:"picture"`
+	Sub           string `json:"sub"`
+}
+
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
+
+func (lc *LoginController) LoginGoogle(c *gin.Context) {
+
+	// Create oauthState cookie
+	oauthState := generateStateOauthCookie(c.Writer)
+
+	googleOauthConfig.ClientID = lc.Env.GoogleOauthClientID
+	googleOauthConfig.ClientSecret = lc.Env.GoogleOauthClientSecret
+
+	u := googleOauthConfig.AuthCodeURL(oauthState)
+
+	fmt.Println("oauthState: ", u)
+
+	http.Redirect(c.Writer, c.Request, u, http.StatusTemporaryRedirect)
+
+}
+
+func (lc *LoginController) LoginGoogleRedirec(c *gin.Context) {
+	// Read oauthState from Cookie
+
+	oauthState, _ := c.Request.Cookie("oauthstate")
+
+	if lc.Env.AppEnv == "development" {
+		if oauthState == nil {
+			oauthState = &http.Cookie{
+				Name:  "oauthstate",
+				Value: c.Query("state"),
+			}
+		}
+	}
+
+	if c.Query("state") != oauthState.Value {
+		log.Println("invalid oauth google state")
+		http.Redirect(c.Writer, c.Request, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	data, err := getUserDataFromGoogle(c.Query("code"))
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, common.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	var response GoogleClaims
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, common.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	// GetOrCreate User in your db.
+	// Redirect or response with a token.
+	// More code .....
+	fmt.Fprintf(c.Writer, "UserInfo: %s\n", data)
+
+	c.JSON(http.StatusOK, response)
+}
+
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	var expiration = time.Now().Add(20 * time.Minute)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	http.SetCookie(w, &cookie)
+
+	return state
+}
+
+func getUserDataFromGoogle(code string) ([]byte, error) {
+	// Use code to get token and get user info from Google.
+
+	token, err := googleOauthConfig.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
+	}
+	response, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+	defer response.Body.Close()
+	contents, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed read response: %s", err.Error())
+	}
+	return contents, nil
 }
 
 func (lc *LoginController) Login(c *gin.Context) {
--- src/server/go.mod
+++ src/server/go.mod
@@ -5,6 +5,7 @@ go 1.24.1
 require github.com/gofiber/fiber/v3 v3.0.0-beta.4
 
 require (
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	github.com/bytedance/sonic v1.12.6 // indirect
 	github.com/bytedance/sonic/loader v0.2.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -81,6 +82,7 @@ require (
 	go.mongodb.org/mongo-driver/v2 v2.2.0
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/oauth2 v0.30.0
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
